01481c58457635861e25bdbd4cc7d1daf22967a4
向林 <as2252258@163.com>
2023-04-16 03:38:47 +0800

    变更

---
+++ src/Validator/BindForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kiri\Router\Validator;
+
+use Kiri\Di\Interface\InjectParameterInterface;
+use Kiri\Router\Interface\ValidatorInterface;
+
+#[\Attribute(\Attribute::TARGET_PARAMETER)]
+class BindForm implements InjectParameterInterface
+{
+
+
+	/**
+	 * @param string $formValidate
+	 */
+	public function __construct(readonly public string $formValidate)
+	{
+	}
+
+
+	/**
+	 * @return mixed
+	 * @throws \ReflectionException
+	 */
+	public function dispatch(): mixed
+	{
+		$validator = new Validator();
+		$reflect = \Kiri::getDi()->getReflectionClass($this->formValidate);
+		$model = $reflect->newInstanceWithoutConstructor();
+		foreach ($reflect->getProperties() as $property) {
+			foreach ($property->getAttributes() as $attribute) {
+				$rule = $attribute->newInstance();
+				if ($rule instanceof ValidatorInterface) {
+					$validator->addRule($property->getName(), $model, $rule);
+				}
+			}
+		}
+		return $validator;
+	}
+
+}
