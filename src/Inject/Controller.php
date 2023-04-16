9cbe8ee062d26b87f0bf85ef51c57fd711fc88c6
向林 <as2252258@163.com>
2023-04-16 16:51:24 +0800

    变更

--- src/Inject/Controller.php
+++ src/Inject/Controller.php
@@ -7,4 +7,20 @@ namespace Kiri\Router\Inject;
 #[\Attribute(\Attribute::TARGET_CLASS)]
 class Controller
 {
+
+	/**
+	 * @throws \ReflectionException
+	 */
+	public function dispatch(object $object): void
+	{
+		$reflection = \Kiri::getDi()->getReflectionClass($object::class);
+		foreach ($reflection->getMethods() as $method) {
+			$attributes = $method->getAttributes();
+			foreach ($attributes as $attribute) {
+				$attribute->newInstance()->dispatch($object, $method->getName());
+			}
+		}
+
+	}
+
 }
